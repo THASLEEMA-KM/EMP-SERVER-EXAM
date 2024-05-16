98bcd26f7699b3d220a598c6bcb7c916de236395
THASLEEMA-KM <thasleemakm5@gmail.com>
2024-05-16 23:29:25 +0530

    Employee Management App server Completed

---
+++ index.js
@@ -0,0 +1,22 @@
+// import json-server
+const jsonServer = require('json-server')
+
+// create server for media player
+const MPServer = jsonServer.create()
+
+// create middleware used by JSON server
+const middleware = jsonServer.defaults()
+
+// set up route fro json file in server
+const route = jsonServer.router('db.json')
+
+// set up pot for running server app
+const PORT = 3001 || process.env.PORT
+
+
+MPServer.use(middleware)
+MPServer.use(route)
+MPServer.listen(PORT,()=>
+{
+    console.log(`Emplloyee app Server Started at Port ${PORT} and waiting for client Request`);
+})
